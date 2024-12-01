53981c52e5ad2cb86e9d1f90a28968422b137e9a
Sven Painer <sven@painer.de>
2024-12-01 16:52:30 +0100

    Day 01.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,35 @@
+import kotlin.math.abs
+
+private fun part1(left: List<Int>, right: List<Int>): Int {
+    return left.sorted().zip(right.sorted())
+        .sumOf { abs(it.first - it.second) }
+}
+
+private fun part2(left: List<Int>, right: List<Int>): Int {
+    val amounts = right.groupBy { it }
+        .mapValues { (_, v) -> v.size }
+
+    return left.sumOf { it * (amounts[it] ?: 0) }
+}
+
+private fun List<String>.toLocationLists(): Pair<List<Int>, List<Int>> {
+    val pairs = map { it.split("\\s+".toRegex()) }
+        .map { arr -> arr.map { it.toInt() } }
+
+    val left = pairs.map { it[0] }
+    val right = pairs.map { it[1] }
+
+    return left to right
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val (testLeft, testRight) = testInput.toLocationLists()
+    check(part1(testLeft, testRight) == 11)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val (left, right) = input.toLocationLists()
+
+    println("Result part 1: ${part1(left, right)}")
+    println("Result part 2: ${part2(left, right)}")
 }
