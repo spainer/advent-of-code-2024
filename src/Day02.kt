8bdbc603988fbef926a24e2609ed269b4010d71d
Sven Painer <sven@painer.de>
2024-12-04 20:14:20 +0100

    Day 02.

---
+++ src/Day02.kt
@@ -0,0 +1,29 @@
+import kotlin.math.abs
+
+private fun part1(reports: List<List<Int>>) = reports.count { it.isSafe }
+
+private fun part2(reports: List<List<Int>>) = reports.count { it.isSafe || it.isCorrectable }
+
+private fun List<String>.toReports(): List<List<Int>> = map { line ->
+    line.split(" ").map { it.toInt() }
+}
+
+private val List<Int>.isSafe: Boolean get() {
+    val diffs = zipWithNext().map { it.second - it.first }
+    return diffs.all { abs(it) in 1..3 } && (diffs.all { it > 0 } || diffs.all { it < 0 })
+}
+
+private val List<Int>.isCorrectable: Boolean get()  = indices.any { (subList(0, it) + subList(it + 1, size)).isSafe }
+
+fun main() {
+    val testInput = readInput("Day02_test")
+    val testReports = testInput.toReports()
+    check(part1(testReports) == 2)
+    check(part2(testReports) == 4)
+
+    val input = readInput("Day02")
+    val reports = input.toReports()
+
+    println("Part 1: ${part1(reports)}")
+    println("Part 2: ${part2(reports)}")
+}
